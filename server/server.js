2dfab2c2c75e56f2b290304bda3b12df55102d64
psalazarAligare <psalazar@aligare.cl>
2019-04-08 17:06:31 -0300

    segundo commit

--- server/server.js
+++ server/server.js
@@ -1,8 +1,21 @@
 const express = require('express')
 const app = express()
 
-app.get('/', function(req, res) {
-    res.json('Hello World')
+app.get('/usuario', function(req, res) {
+    res.json('get Usuario')
+})
+
+app.post('/usuario', function(req, res) {
+    res.json('post Usuario')
+})
+
+app.put('/usuario/:id', function(req, res) {
+    let idx = req.params.id;
+    res.json({ idx })
+})
+
+app.delete('/usuario', function(req, res) {
+    res.json('delete Usuario')
 })
 
 app.listen(8080, () => {
