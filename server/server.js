56a970db788639ce174a017748e615324a8bec8b
Raxon90 <p.salazar.oso@gmail.com>
2019-06-02 14:16:57 -0400

    primer commit

--- server/server.js
+++ server/server.js
@@ -1,12 +1,30 @@
+require('./config/config');
+
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+    // parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
 
 app.get('/usuario', function(req, res) {
     res.json('get Usuario')
 })
 
 app.post('/usuario', function(req, res) {
-    res.json('post Usuario')
+    let body = req.body;
+    if (body.nombre === undefined) {
+        res.status(400).json({
+            ok: false,
+            mensaje: 'el nombre es necesario'
+        });
+    } else {
+        res.json({
+            persona: body
+        });
+    }
 })
 
 app.put('/usuario/:id', function(req, res) {
@@ -18,6 +36,6 @@ app.delete('/usuario', function(req, res) {
     res.json('delete Usuario')
 })
 
-app.listen(8080, () => {
-    console.log('Escuchando puerto 8080');
+app.listen(process.env.PORT, () => {
+    console.log('Escuchando puerto: ', process.env.PORT);
 })
